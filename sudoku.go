62d427f22a146c750c264904be007d54691b8dd1
Max Tremaine <max.tremaine@gmail.com>
2023-01-26 10:47:41 -0500

    Got to identifying duplicates

--- sudoku.go
+++ sudoku.go
@@ -38,7 +38,14 @@ type sudoku struct {
 }
 
 func (puzzle sudoku) isValid() bool {
-	
+
+	for group := range groups {
+		var groupCells [9]int
+
+		for cellIndex := range group {
+
+		}
+	}
 
 	return len(groups) == 81
 }
