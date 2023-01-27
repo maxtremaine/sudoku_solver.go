52e8670cec690750574428ce29039ced34eb91f7
maxtremaine <max.tremaine@gmail.com>
2023-01-27 15:34:16 -0500

    Used go fmt

--- sudoku_test.go
+++ sudoku_test.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 var validFile = `  abc def ghi
@@ -18,9 +18,9 @@ var validFile = `  abc def ghi
 8 __8|___|4__
 9 6__|_2_|__8`
 
-var validPuzzle = sudoku{ 7, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0, 2, 0,
+var validPuzzle = sudoku{7, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0, 2, 0,
 	9, 0, 8, 0, 0, 0, 3, 5, 0, 4, 9, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 2, 1, 0, 8, 5, 0,
-	0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0, 0, 0, 2, 0, 0, 0, 8 }
+	0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0, 0, 0, 2, 0, 0, 0, 8}
 
 func TestSudokuFromString(t *testing.T) {
 	validSudoku, err := sudokuFromString(validFile)
@@ -39,9 +39,9 @@ func TestIsValid(t *testing.T) {
 		t.Error("isValid() expected true.")
 	}
 
-	invalidPuzzle := sudoku{ 7, 7, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0, 2,
+	invalidPuzzle := sudoku{7, 7, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0, 2,
 		0, 9, 0, 8, 0, 0, 0, 3, 5, 0, 4, 9, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 2, 1, 0, 8, 5,
-		0, 0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0, 0, 0, 2, 0, 0, 0, 8 }
+		0, 0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0, 0, 0, 2, 0, 0, 0, 8}
 	output = invalidPuzzle.isValid()
 	if output != false {
 		t.Error("isValid() expected false.")
@@ -50,7 +50,7 @@ func TestIsValid(t *testing.T) {
 
 func TestGetRelatedCells(t *testing.T) {
 	relatedToValid := validPuzzle.getRelatedCells(1)
-	relatedToOne := []int{ 1, 4, 6, 7 }
+	relatedToOne := []int{1, 4, 6, 7}
 	if !reflect.DeepEqual(relatedToValid, relatedToOne) {
 		t.Errorf("getRelatedCells expected %d, got %d.", relatedToOne, relatedToValid)
 	}
--- utilities_test.go
+++ utilities_test.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestLastIndex(t *testing.T) {
-	indexIsFive := lastIndex([]int{ 1, 2, 3, 3, 3, 3 }, 3)
+	indexIsFive := lastIndex([]int{1, 2, 3, 3, 3, 3}, 3)
 	if indexIsFive != 5 {
 		t.Errorf("lastIndex Expected 5, received %d.", indexIsFive)
 	}
-	indexNeg := lastIndex([]int{ 1, 2, 3 }, 5)
+	indexNeg := lastIndex([]int{1, 2, 3}, 5)
 	if indexNeg != -1 {
 		t.Errorf("lastIndex expected -1, received %d.", indexNeg)
 	}
 }
 
 func TestHasDuplicates(t *testing.T) {
-	includesDuplicates := []int{ 1, 2, 3, 3 }
+	includesDuplicates := []int{1, 2, 3, 3}
 	shouldBeTrue := hasDuplicates(includesDuplicates)
 	if shouldBeTrue != true {
 		t.Errorf("hasDuplicates expected true, received %t.", shouldBeTrue)
 	}
-	allUnique := []int{ 1, 2, 3, 4 }
+	allUnique := []int{1, 2, 3, 4}
 	shouldBeFalse := hasDuplicates(allUnique)
 	if shouldBeFalse != false {
 		t.Errorf("hasDuplicates expected false, received %t.", shouldBeFalse)
@@ -30,8 +30,8 @@ func TestHasDuplicates(t *testing.T) {
 }
 
 func TestUniqueNumbers(t *testing.T) {
-	testList := []int{ 4, 4, 5, 5, 5, 1, 2, 3 }
-	outputList := []int{ 1, 2, 3, 4, 5 }
+	testList := []int{4, 4, 5, 5, 5, 1, 2, 3}
+	outputList := []int{1, 2, 3, 4, 5}
 	uniqueOutput := uniqueNumbers(testList)
 	if !reflect.DeepEqual(uniqueOutput, outputList) {
 		t.Errorf("uniqueNumbers expected %d, received %d.", outputList, uniqueOutput)
